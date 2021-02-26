ca7c72f1c0227255da0e9848a2518f5428821951
opensprocket <opensprocket@github.com>
2021-02-26 11:51:29 -0700

    v2

--- raspi-setup.sh
+++ raspi-setup.sh
@@ -1,17 +1,8 @@
 #!/bin/bash -e
 
-# Prompt for Hostname
-read -p "Please input the hostname of the machine:" -r -s HOSTNAME
-
-sudo hostnamectl set-hostname $HOSTNAME
-
-echo "Hostname has been changed successfully!" 
-echo "Close and reopen terminal once script finishes to verify the change."
-
 # Set locale
 echo -e "en_US.UTF-8 UTF-8" | sudo tee -a /etc/locale.gen
 sudo locale-gen
-
 
 # Update, remove bloat + install apps
 sudo apt update -y \
@@ -23,3 +14,40 @@ sudo apt autoremove -y
 # Disable screensleep
 echo '@xset s off' | sudo tee -a /etc/xdg/lxsession/LXDE-pi/autostart
 echo '@xset -dpms' | sudo tee -a /etc/xdg/lxsession/LXDE-pi/autostart
+
+#
+# Create doterrala
+#
+sudo useradd -g users -G adm,dialout,cdrom,sudo,audio,video,plugdev,games,input,netdev,lpadmin,gpio,i2c,spi doterrala
+
+# Set password
+read -p "Password for doterrala:" -r -s PASS
+(echo -e $PASS; echo -e $PASS) | sudo passwd doterrala
+
+
+#
+# Disable pi user
+#
+
+# Remove pi from sudo group
+sudo gpasswd -d pi sudo
+
+# Remove pi sudoers file
+sudo rm /etc/sudoers.d/010_pi-nopasswd
+
+# Disable password for pi
+sudo passwd -l pi
+
+
+# Prompt for Hostname - Make sure this is the last thing to do!
+read -p "Please input the hostname of the machine:" -r -s HOSTNAME
+
+sudo hostnamectl set-hostname $HOSTNAME
+
+echo "Hostname has been changed successfully!" 
+echo "Close and reopen terminal once script finishes to verify the change."
+
+# Reboot
+echo "This machine will reboot in 5 seconds..."
+sleep 5
+sudo reboot
