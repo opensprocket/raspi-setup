9cee0fa4ff45ffd9640b2ea4af5a8e958c9ce99a
opensprocket <opensprocket@users.noreply.github.com>
2021-03-04 15:07:20 -0700

    fixes + additions
    fixed install/update section, added vnc startup, reworked user account creation to make an account that actually works :octocat:

--- raspi-setup.sh
+++ raspi-setup.sh
@@ -1,13 +1,15 @@
 #!/bin/bash -e
 
+#
+
 # Set locale
 echo -e "en_US.UTF-8 UTF-8" | sudo tee -a /etc/locale.gen
 sudo locale-gen
 
 # Update, remove bloat + install apps
-sudo apt update -y \
-sudo apt remove thonny geany -y \
-sudo apt install libreoffice syncthing syncthing-gtk -y --no-install-recommends \
+sudo apt update -y
+sudo apt remove thonny geany -y
+sudo apt install libreoffice syncthing syncthing-gtk -y --no-install-recommends
 sudo apt autoremove -y
 
 
@@ -16,14 +18,27 @@ echo '@xset s off' | sudo tee -a /etc/xdg/lxsession/LXDE-pi/autostart
 echo '@xset -dpms' | sudo tee -a /etc/xdg/lxsession/LXDE-pi/autostart
 
 #
+# Enable VNC
+#
+
+sudo apt install realvnc-vnc-server -y
+systemctl enable vncserver-x11-serviced.service &&
+systemctl start vncserver-x11-serviced.server
+echo "VNC has been enabled for this machine."
+
+
+#
 # Create doterrala
 #
-sudo useradd -g users -G adm,dialout,cdrom,sudo,audio,video,plugdev,games,input,netdev,lpadmin,gpio,i2c,spi doterrala
 
-# Set password
+# Get password
 read -p "Password for doterrala:" -r -s PASS
-(echo -e $PASS; echo -e $PASS) | sudo passwd doterrala
+# (echo -e $PASS; echo -e $PASS) | sudo passwd doterrala
 
+# Create user account
+sudo useradd -p $(openssl passwd -1 $PASS) doterrala
+
+sudo usermod -G adm,dialout,cdrom,sudo,audio,video,plugdev,games,input,netdev,lpadmin,gpio,i2c,spi doterrala
 
 #
 # Disable pi user
@@ -36,7 +51,7 @@ sudo gpasswd -d pi sudo
 sudo rm /etc/sudoers.d/010_pi-nopasswd
 
 # Disable password for pi
-sudo passwd -l pi
+# sudo passwd -l pi
 
 
 # Prompt for Hostname - Make sure this is the last thing to do!
