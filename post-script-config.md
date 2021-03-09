87134f13c8e54f8091b0f98b9ab8d7c4b988c9f9
opensprocket <opensprocket@users.noreply.github.com>
2021-03-09 13:09:48 -0700

    rc 3

---
+++ post-script-config.md
@@ -0,0 +1,28 @@
+## doterrala Creation
+
+Create account (enter password when prompted):
+```
+sudo useradd doterrala
+```
+Update Group membership (one line):
+```
+sudo usermod -G adm,dialout,cdrom,sudo,audio,video,plugdev,games,input,netdev,lpadmin,gpio,i2c,spi doterrala
+```
+
+## Update `pi` User
+
+Set password for `pi`:
+```
+sudo passwd pi
+```
+
+Remove `pi` from sudo group
+```
+sudo gpasswd -d pi sudo
+```
+
+Remove `pi` sudoers file:
+```
+sudo rm /etc/sudoers.d/010_pi-nopasswd
+```
+
--- raspi-setup.sh
+++ raspi-setup.sh
@@ -15,52 +15,6 @@ sudo apt autoremove -y
 echo '@xset s off' | sudo tee -a /etc/xdg/lxsession/LXDE-pi/autostart
 echo '@xset -dpms' | sudo tee -a /etc/xdg/lxsession/LXDE-pi/autostart
 
-#
-# Create doterrala
-#
-
-# Get password
-read -p "Password for doterrala:" -r -s PASS
-# (echo -e $PASS; echo -e $PASS) | sudo passwd doterrala
-
-# Create user account
-sudo useradd -p $(openssl passwd -1 $PASS) doterrala
-
-sudo usermod -G adm,dialout,cdrom,sudo,audio,video,plugdev,games,input,netdev,lpadmin,gpio,i2c,spi doterrala
-
-
-
-#
-# Enable VNC
-#
-
-sudo apt install realvnc-vnc-server -y
-sudo systemctl enable vncserver-x11-serviced.service &&
-sudo systemctl start vncserver-x11-serviced.server
-echo "VNC has been enabled for this machine."
-
-#
-# Disable pi user
-#
-
-# Remove pi from sudo group
-sudo gpasswd -d pi sudo
-
-# Remove pi sudoers file
-sudo rm /etc/sudoers.d/010_pi-nopasswd
-
-# Disable password for pi
-# sudo passwd -l pi
-
-
-# Prompt for Hostname - Make sure this is the last thing to do!
-# read -p "Please input the hostname of the machine:" -r -s HOSTNAME
-
-# sudo hostnamectl set-hostname $HOSTNAME
-
-# echo "Hostname has been changed successfully!" 
-# echo "Close and reopen terminal once script finishes to verify the change."
-
 # Reboot
 echo "This machine will reboot in 5 seconds..."
 sleep 5
