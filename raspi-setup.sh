1d200d5d1578725e4e107a7110e19e8c348497ba
Josh <opensprocket@users.noreply.github.com>
2021-08-16 13:19:30 -0600

    added .bash_aliases

--- raspi-setup.sh
+++ raspi-setup.sh
@@ -1,11 +1,15 @@
 #!/bin/bash -e
 
+# Configure .bash_aliases
+sudo touch ~/.bash_aliases
+echo 'alias cls="clear"' | sudo tee -a ~/.bash_aliases
+echo 'alias ll="ls -al"' | sudo tee -a ~/.bash_aliases
+
 # Update, remove bloat + install apps
 sudo apt update -y
 sudo apt remove thonny geany -y
 sudo apt install libreoffice syncthing syncthing-gtk -y --no-install-recommends
 sudo apt autoremove -y
-
 
 # Disable screensleep
 echo "Disabling screensleep..."
